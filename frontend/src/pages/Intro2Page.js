b7eeb9cddfbd7d945fce7ee60275197d1d0b5e6d
Chansol Lim <lchans0319@gmail.com>
2022-02-15 18:29:56 +0900

    도서관 소개2(오시는 길), 도서관 이용1(이용시간) 추가

--- frontend/src/pages/Intro2Page.js
+++ frontend/src/pages/Intro2Page.js
@@ -1,6 +1,51 @@
+import entrance from "../images/entrance.jpg";
+
 function Intro2Page() {
   return (
-    <div>인트로2 페이지</div>
+    <>
+      <h2>도서관 소개2</h2>
+      <div id="contents">
+        <div id="directions">
+          <h3>오시는 길</h3>
+          <img src={entrance} alt="bookarkhaneul_entrance" width="500px" />
+          <div id="address">
+            <h4>도로명주소</h4>
+            <p>
+              (02821) 서울특별시 성북구 북악산로1다길 26 작은도서관/문화공간
+              북악하늘
+            </p>
+            <h4>지번주소</h4>
+            <p>
+              (02821) 서울특별시 성북구 정릉동 508-97 작은도서관/문화공간
+              북악하늘
+            </p>
+          </div>
+          <div id="public-transportation">
+            <h4>대중교통 이용 방법</h4>
+            <p>
+              지하철 4호선 성신여대역 6번출구 혹은 6호선 보문역 4번출구로
+              나와서, 시내버스 1162를 타고 508 스카이 단지 내 '골드하우스'
+              정류장에 내려주세요. <br />
+              버스 반대 방향으로 30미터 오시면 북악하늘이 있습니다.
+            </p>
+          </div>
+        </div>
+        <div id="connect-number">
+          <h4>연락처</h4>
+          <p>
+            북악하늘: 070-8806-0205
+            <br />
+            임명진 관장: 010-4258-8205
+            <br />
+            팩스: 09-942-8205
+            <br />
+            이메일: duck0624@naver.com
+            <br />
+            블로그: www.blog.naver.com/duck0624
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
 
--- frontend/src/pages/Rule1Page.js
+++ frontend/src/pages/Rule1Page.js
@@ -1,6 +1,52 @@
 function Rule1Page() {
   return (
-    <div>규칙1 페이지</div>
+    <>
+      <h2>도서관 이용 안내</h2>
+      <div id="contents">
+        <div id="service-users" className="container">
+          <h3>이용 대상</h3>
+          <ul>
+            <li>자료 열람을 원하는 누구나 이용 가능합니다.</li>
+            <li>
+              단, 대출/예약 등 일부 서비스는 북악하늘 작은도서관 회원만 이용
+              가능합니다.
+            </li>
+          </ul>
+        </div>
+        <div id="service-hours" className="container">
+          <h3>이용 시간</h3>
+          <p>
+            이용 시간은 조금씩 변동될 수 있으며, <br /> 문의 시 협의 하에 이용
+            시간 일시적으로 변동 가능합니다.
+          </p>
+          <table>
+            <tr>
+              <td></td>
+              <td>하절기(4월 ~ 10월)</td>
+              <td>동절기(11월 ~3월)</td>
+            </tr>
+            <tr>
+              <td>여는 시간</td>
+              <td>오전 11:30</td>
+              <td>오후 12:30</td>
+            </tr>
+            <tr>
+              <td>닫는 시간</td>
+              <td>오후 21:00</td>
+              <td>오후 20:00</td>
+            </tr>
+          </table>
+        </div>
+        <div id="closed-day">
+          <h3>휴관일</h3>
+          <ul>
+            <li>매주 월요일과 일요일</li>
+            <li>법정 공휴일</li>
+            <li>기타 관장이 필요하다고 인정하는 날 미리 공지 후 휴관</li>
+          </ul>
+        </div>
+      </div>
+    </>
   );
 }
 
