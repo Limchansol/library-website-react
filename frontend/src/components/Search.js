ba21713a6cc5377d2ec1a6c8696d51b487aa7965
Chansol Lim <lchans0319@gmail.com>
2022-02-15 15:07:02 +0900

    검색 쿼리 카테고리로 변경

--- frontend/src/components/Search.js
+++ frontend/src/components/Search.js
@@ -1,29 +1,47 @@
-import { useState } from 'react';
-import { Link } from 'react-router-dom';
-import logoImg from '../images/main_logo.png';
-import './Search.css';
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import logoImg from "../images/main_logo.png";
+import "./Search.css";
 
 function Search() {
+  const [category, setCategory] = useState("all");
+  const [keyword, setKeyword] = useState("");
 
-  const [keyword, setKeyword] = useState('');
-
-  const handleInputChange = (e) => { setKeyword(e.target.value); }
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+  const handleKeywordChange = (e) => {
+    setKeyword(e.target.value);
+  };
 
   return (
-    <div id='header-middle'>
+    <div id="header-middle">
       <h1 className="logo">
-        <Link to="/"><img src={logoImg} alt="북악하늘_작은도서관" height="80px" /></Link>
+        <Link to="/">
+          <img src={logoImg} alt="북악하늘_작은도서관" height="80px" />
+        </Link>
       </h1>
       <form className="search-form">
-        <select className="search-criteria">
+        <select className="search-criteria" onChange={handleCategoryChange}>
           <option value="all">통합</option>
           <option value="title">제목</option>
           <option value="writer">지은이</option>
           <option value="publisher">출판사</option>
           <option value="ISBN">ISBN</option>
         </select>
-        <input type="search" value={keyword} onChange={handleInputChange} className="search-input" placeholder="원하는 도서를 검색하세요." required autoComplete='off' />
-        <Link to={`/searched?keyword=${keyword}`} state={{keyword: keyword}}>
+        <input
+          type="search"
+          value={keyword}
+          onChange={handleKeywordChange}
+          className="search-input"
+          placeholder="원하는 도서를 검색하세요."
+          required
+          autoComplete="off"
+        />
+        <Link
+          to={`/searched?${category}=${keyword}`}
+          state={{ keyword: keyword, category: category }}
+        >
           <button className="search-btn">검색</button>
         </Link>
       </form>
--- frontend/src/pages/SearchedPage.js
+++ frontend/src/pages/SearchedPage.js
@@ -3,29 +3,42 @@ import { useLocation } from "react-router-dom";
 import axios from "axios";
 
 function SearchedPage() {
+  const category = useLocation().state.category;
   const keyword = useLocation().state.keyword;
   const [selectedBooks, setSelectedBooks] = useState([]);
 
-  function filterByKeyword(keyword, items) {
+  // 서버에서 find 처리하면 이 함수는 필요없을듯
+  function filterByKeyword(category, keyword, items) {
     const lowered = keyword.toLowerCase();
-    return items.filter(
-      (item) =>
-        item.title.toLowerCase().includes(lowered) ||
-        item.keyword.toLowerCase().includes(lowered)
-    );
+    let filteredItems;
+    if (category === "all") {
+      filteredItems = items.filter(
+        (item) =>
+          item.title.toLowerCase().includes(lowered) ||
+          item.writer.toLowerCase().includes(lowered) ||
+          item.publisher.toLowerCase().includes(lowered) ||
+          item.ISBN.toLowerCase().includes(lowered) ||
+          item.keyword.toLowerCase().includes(lowered)
+      );
+    } else {
+      filteredItems = items.filter((item) =>
+        item[category].toLowerCase().includes(lowered)
+      );
+    }
+    return filteredItems;
   }
 
   useEffect(() => {
     const fetchData = async () => {
       const fetchedData = await axios.get("api/books");
       const books = fetchedData.data;
-      const wantedBooks = filterByKeyword(keyword, books);
+      const wantedBooks = filterByKeyword(category, keyword, books);
       setSelectedBooks(wantedBooks);
     };
     fetchData();
-  }, [keyword]);
+  }, [category, keyword]);
 
-  if (!keyword) {
+  if (!keyword || selectedBooks.length === 0) {
     return (
       <>
         <div>자료1페이지</div>
