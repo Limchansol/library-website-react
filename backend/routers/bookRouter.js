f9c36d3d60a71bdf7d67d7a0744dc808071091db
wonwoo <jww0418@snu.ac.kr>
2022-02-22 17:32:21 +0900

    1. 검색어에 특수문자가 포함될 수 있도록 router쪽 logic추가 2. 상세 검색 페이지 추가

--- backend/routers/bookRouter.js
+++ backend/routers/bookRouter.js
@@ -60,6 +60,38 @@ bookRouter.get(
 );
 
 bookRouter.get(
+  "/detailed",
+  expressAsyncHandler(async (req, res) => {
+    const books = await Book.find({
+      $and: [
+        {
+          title: {
+            $regex: req.query.title.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
+          }, //정규표현식에서 특수문자를 escapse해주도록 하는 간단한 로직을 추가!!!!!!
+        },
+        {
+          writer: {
+            $regex: req.query.writer.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
+          },
+        },
+        {
+          publisher: {
+            $regex: req.query.publisher.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
+          },
+        },
+        {
+          ISBN: {
+            $regex: req.query.isbn.replace(/[.*+?^${}()|[\]\\]/g, "\\$&"),
+          },
+        },
+      ],
+    }); //큰 버그가 존재한다! 검색시 특수기호를 포함한 쿼리가 있으면 그것을 인코딩하고 보내야 한다! 반드시 수정할 것.
+    //또한 isbn같은 경우, 정확한 검색일 때에만 검색할 수 있도록 만들어 줘야 할 것이다!
+    res.send(books);
+  })
+);
+
+bookRouter.get(
   "/seed",
   expressAsyncHandler(async (req, res) => {
     const books = await Book.insertMany(data.books);
--- frontend/src/pages/DetailedSearchPage.js
+++ frontend/src/pages/DetailedSearchPage.js
@@ -1,9 +1,99 @@
+import axios from "axios";
+import { useState } from "react";
+import "./DetailedSearchPage.css";
+
 function DetailedSearchPage() {
+  const [detailedSearchValue, setDetailedSearchValue] = useState({
+    title: "",
+    writer: "",
+    isbn: "",
+    publisher: "",
+  });
+
+  const handleDetailedSearchValue = (e) => {
+    const { id, value } = e.target;
+    setDetailedSearchValue({
+      ...detailedSearchValue,
+      [id]: value,
+    });
+  };
+
+  const fetchDetailedBook = async (
+    title = "",
+    writer = "",
+    isbn = "",
+    publisher = ""
+  ) => {
+    const encodedQuery = [
+      encodeURIComponent(title),
+      encodeURIComponent(writer),
+      encodeURIComponent(isbn),
+      encodeURIComponent(publisher),
+    ];
+    const books = await axios.get(
+      `/api/books/detailed?title=${encodedQuery[0]}&writer=${encodedQuery[1]}&isbn=${encodedQuery[2]}&publisher=${encodedQuery[3]}`
+    );
+    return books;
+  };
+
+  const detailedSearch = (e) => {
+    e.preventDefault();
+    const { title, writer, isbn, publisher } = detailedSearchValue;
+    const books = fetchDetailedBook(title, writer, isbn, publisher);
+    console.log(books);
+  };
+
   return (
     <>
-      <div>상세검색 페이지</div>
+      <div id="detailedSearchtext">
+        <h1>상세검색이란?</h1>
+        <p>
+          한 번에 여러 개의 요소를 검색할 수 있고, 그것에 <strong>모두 </strong>
+          해당하는 책을 보여줍니다.
+          <br />
+          가령, 저자를 'ㄱ', 도서명을 'ㄴ'으로 검색하면 저자가 ㄱ이면서 도서명이
+          ㄴ인 책을 보여줍니다.
+        </p>
+      </div>
+      <div id="detailedSearchForm">
+        <form onSubmit={detailedSearch}>
+          <label htmlFor="title">도서명: </label>
+          <input
+            type="text"
+            id="title"
+            onChange={handleDetailedSearchValue}
+            value={detailedSearchValue.title}
+          />
+          <br />
+          <label htmlFor="writer">저자: </label>
+          <input
+            type="text"
+            id="writer"
+            onChange={handleDetailedSearchValue}
+            value={detailedSearchValue.writer}
+          />
+          <br />
+          <label htmlFor="isbn">ISBN: </label>
+          <input
+            type="text"
+            id="isbn"
+            onChange={handleDetailedSearchValue}
+            value={detailedSearchValue.isbn}
+          />
+          <br />
+          <label htmlFor="publisher">출판사: </label>
+          <input
+            type="text"
+            id="publisher"
+            onChange={handleDetailedSearchValue}
+            value={detailedSearchValue.publisher}
+          />
+          <br />
+          <button id="searchButton">검색</button>
+        </form>
+      </div>
     </>
   );
-}
+} //추후 수정
 
 export default DetailedSearchPage;
